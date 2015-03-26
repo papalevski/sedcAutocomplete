cdf5951ccbb241e99dc32d067d025219077e2caa
petar.papalevski <petar.papalevski@haselt.com>
2015-03-26 19:06:31 +0100

    Add autocomplete plugin without jqueryui

--- sedcAutocomplete.js
+++ sedcAutocomplete.js
@@ -23,4 +23,37 @@
         return this;
     };
 
+    $.fn.sedcAutocompleteWithoutJqueryUI = function () {
+
+
+        $(this).keypress(function () {
+
+            var searchText = $(this).val();
+
+            var resultsPanel = $(this).attr("data-results-panel-id");
+
+            $.ajax({
+                url: "http://en.wikipedia.org/w/api.php",
+                dataType: "jsonp",
+                data: {
+                    'action': "opensearch",
+                    'format': "json",
+                    'search': searchText
+                },
+                success: function (data) {
+
+                    $("#" + resultsPanel).html("");
+
+                    $.each(data[1], function (i, item) {
+                        $("#" + resultsPanel).append("<p>" + item + "</p>");
+                    });
+
+                }
+            });
+        });
+
+
+        return this;
+    };
+
 }(jQuery));
